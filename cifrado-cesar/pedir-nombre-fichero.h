7b3fcc94649d1f1a5f6d2ffa2537520a0254f503
Miguel Latre <latre@unizar.es>
2019-12-16 15:55:10 +0100

    Update pedir-nombre-fichero.h

--- cifrado-cesar/pedir-nombre-fichero.h
+++ cifrado-cesar/pedir-nombre-fichero.h
@@ -20,23 +20,42 @@ const char DIR_RESULTADOS[] = "../../resultados/";
 /*
  * Pre:  «rutaOrigen» y «rutaDestino» tienen una dimensión suficiente como
  *       para albergar una ruta relativa a un fichero cuya denominación
- *       escribirá el usuario.
- * Post: Ha solicitado al usuario el nombre de un fichero, utilizando para
- *       ello la cadena de caracteres «mensaje». A continuación, a leído dicha
- *       denominación del teclado (consistente en un nombre base que no
- *       contenía el carácter «.» y una extensión que comienza con el
- *       carácter «.»). Ha asignado a «rutaOrigen» una ruta de acceso relativa
- *       al mismo, consistente en la concatenación de los siguientes
- *       elementos:
- *          - la cadena «DIR_DATOS»
- *          - el nombre base del fichero leído del teclado
+ *       escribirá el usuario. Sus valores están indeterminados al comenzar a
+ *       ejecutarse esta función.
+ * 
+ * Post: Cuando ha terminado de ejecutarse esta función, se ha solicitado al
+ *       usuario el nombre de un fichero, utilizando como mensaje para realizar
+ *       dicha solicitud la cadena de caracteres «mensaje», que se ha escrito en
+ *       la pantalla.
+ * 
+ *       A continuación, ha leído el nombre del fichero del teclado. (Como este 
+ *       consta de un nombre base que no contiene el carácter «.» y una extensión
+ *       que comienza con el carácter «.», se ha podido leer con dos instrucciones
+ *       distintas, utilizando «.» como delimitador en la primera de ellas.)
+ * 
+ *       Ha asignado a «rutaOrigen» una ruta de acceso relativa
+ *       al fichero cuyo nombre se ha leído de teclado, consistente en la
+ *       concatenación de los siguientes elementos:
+ *          - la cadena «DIR_DATOS»,
+ *          - el nombre base del fichero leído del teclado,
  *          - la extensión del fichero leído del teclado.
+ * 
  *       Ha asignado a «rutaDestino» una ruta de acceso a un fichero
  *       consistente en la concatenación de los siguientes elementos:
- *          - la cadena «DIR_RESULTADOS»
- *          - el nombre base del fichero leído del teclado
- *          - el interfijo establecido por el valor del parámetro «interfijo»
+ *          - la cadena «DIR_RESULTADOS»,
+ *          - el nombre base del fichero leído del teclado,
+ *          - el interfijo establecido por el valor del parámetro «interfijo»,
  *          - la extensión del fichero leído del teclado.
+ * 
+ * Ejemplo de utilización: cuando se implemente en el módulo principal la opción 1
+ * (cifrado de un texto), se puede invocar a esta función utilizando como
+ * argumento para el parámetro «mensaje» la cadena de caracteres
+ * "Nombre del fichero a cifrar: " y como argumento para el parámetro «interfijo»,
+ * la cadena "-cifrado". Cuando se haya ejecutado la función, si el usuario ha
+ * escrito "quijote.txt", el valor asignado al argumento correspondiente al
+ * parámetro de salida «rutaOrigen» será "../../datos/quijote.txt" y, el valor
+ * asignado al argumento correspondiente al parámetro de salida «rutaDestino»
+ * será "../../resultados/quijote-cifrado.txt".
  */
 void pedirNombreFichero(const char mensaje[], const char interfijo[],
                         char rutaOrigen[], char rutaDestino[]);
